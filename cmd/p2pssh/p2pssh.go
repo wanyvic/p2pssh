1b01c7589f41a710cf3c1b51f9faf20af46343ac
wanyvic <q873040807@gmail.com>
2019-08-23 18:57:13 +0800

    ad

---
+++ cmd/p2pssh/p2pssh.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/wanyvic/p2pssh/cmd"
+)
+
+func main() {
+
+	// initial log formatting; this setting is updated after the daemon configuration is loaded.
+	logrus.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000000000Z07:00",
+		FullTimestamp:   true,
+	})
+
+	if err := cmd.Execute(); err != nil {
+		logrus.Println(err)
+	}
+	logrus.Debug("pssh exit")
+}
