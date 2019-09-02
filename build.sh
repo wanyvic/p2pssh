f9e5ea50dbc93ea8abebdf26ab153b83bcbfb9c8
wanyvic <q873040807@gmail.com>
2019-09-02 10:24:52 +0800

    fixed untracked files

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+project_path=$(cd `dirname $0`; pwd)
+mkdir -p "${project_path}/version"
 GITCOMMIT=$(git rev-parse --short HEAD)
 VERSION=$(git describe --abbrev=0 --tags)
 BUILDTIME=$(date -u -d "@${SOURCE_DATE_EPOCH:-$(date +%s)}" --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
-cat > version/version.go <<DVEOF
+cat > ${project_path}/version/version.go <<DVEOF
 // +build autogen
 // Package version is auto-generated at build-time
 package version
@@ -14,5 +16,4 @@ const (
 	BuildTime             string = "$BUILDTIME"
 )
 DVEOF
-
-go build -work ./cmd/p2pssh/p2pssh.go
+go build -work ${project_path}/cmd/p2pssh/p2pssh.go
